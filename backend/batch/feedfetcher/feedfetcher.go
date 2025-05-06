160c564216e157c05c529d9680f02ab3d78fe1b8
Kotaro Abe <abekoh.bcky@gmail.com>
2025-05-06 21:18:23 +0900

    fix: Move feedfetcher import to batch package
    This commit moves the feedfetcher import from the lib package to the batch package.
    
    The changes include:
    
    - Moves the import of `github.com/abekoh/simple-rss/backend/lib/feedfetcher` to `github.com/abekoh/simple-rss/backend/batch/feedfetcher` in `backend/domain/service/feed.go`.

---
+++ backend/batch/feedfetcher/feedfetcher.go
@@ -0,0 +1,77 @@
+package feedfetcher
+
+import (
+	"context"
+
+	"github.com/mmcdole/gofeed"
+)
+
+type (
+	Request struct {
+		URL string
+	}
+
+	requestWithResultCh struct {
+		Request
+		ResultCh chan *Result
+	}
+
+	Result struct {
+		Success bool
+		Content *gofeed.Feed
+		Error   error
+	}
+)
+
+type FeedFetcher struct {
+	feedParser *gofeed.Parser
+	requestCh  chan *requestWithResultCh
+}
+
+func NewFeedFetcher(ctx context.Context) *FeedFetcher {
+	c := &FeedFetcher{
+		feedParser: gofeed.NewParser(),
+		requestCh:  make(chan *requestWithResultCh, 10),
+	}
+	go c.Loop(ctx)
+	return c
+}
+
+func (c FeedFetcher) Loop(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case req := <-c.requestCh:
+			feedContent, err := c.feedParser.ParseURL(req.URL)
+			if err != nil {
+				req.ResultCh <- &Result{
+					Success: false,
+					Error:   err,
+				}
+				continue
+			}
+			req.ResultCh <- &Result{
+				Success: true,
+				Content: feedContent,
+			}
+		}
+	}
+}
+
+func (c FeedFetcher) SendRequest(req Request) (*Result, error) {
+	resCh := make(chan *Result)
+	c.requestCh <- &requestWithResultCh{
+		Request:  req,
+		ResultCh: resCh,
+	}
+	return <-resCh, nil
+}
+
+var (
+	DefaultFeedFetcher = NewFeedFetcher(context.Background())
+)
+
+func SendRequest(req Request) (*Result, error) {
+	return DefaultFeedFetcher.SendRequest(req)
+}
