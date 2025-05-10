7ff56d4092537f56fd5f8fbd46a9ae9e5822bc83
Kotaro Abe <abekoh.bcky@gmail.com>
2025-05-10 11:49:04 +0900

    feat: update database schema with new tables and indexes

--- backend/migrations/20250506015707_initial_tables.sql
+++ backend/migrations/20250506015707_initial_tables.sql
@@ -1,46 +1,72 @@
 -- +goose Up
-CREATE TABLE feeds (
-    feed_id uuid primary key,
-    url text not null unique,
-    title text not null,
-    description text,
+CREATE TABLE feeds
+(
+    feed_id       uuid primary key,
+    url           text                     not null unique,
+    title         text                     not null,
+    description   text,
     registered_at timestamp with time zone not null,
-    created_at timestamp with time zone not null default now()
+    created_at    timestamp with time zone not null default now()
 );
 
-CREATE TYPE crawl_status AS ENUM ('Success', 'Failure');
+CREATE INDEX feeds_url_idx ON feeds (url);
 
-CREATE TABLE crawls (
-    crawl_id uuid primary key,
-    feed_id uuid not null references feeds(feed_id),
-    status crawl_status not null,
-    message text,
-    crawled_at timestamp with time zone not null,
-    created_at timestamp with time zone not null default now()
+CREATE TYPE feed_fetch_status AS ENUM ('Success', 'Failure');
+
+CREATE TABLE feed_fetches
+(
+    feed_fetch_id uuid primary key,
+    feed_id       uuid                     not null references feeds (feed_id),
+    status        feed_fetch_status        not null,
+    message       text,
+    fetched_at    timestamp with time zone not null,
+    created_at    timestamp with time zone not null default now()
 );
 
-CREATE TABLE posts (
-    post_id uuid primary key,
-    feed_id uuid not null references feeds(feed_id),
-    crawl_id uuid not null references crawls(crawl_id),
-    title text not null,
-    author text,
-    url text not null unique,
-    summary_original text,
-    posted_at timestamp with time zone not null,
-    created_at timestamp with time zone not null default now()
+CREATE TABLE posts
+(
+    post_id     uuid primary key,
+    feed_id     uuid                     not null references feeds (feed_id),
+    title       text                     not null,
+    description text,
+    author      text,
+    url         text                     not null unique,
+    posted_at   timestamp with time zone not null,
+    created_at  timestamp with time zone not null default now()
 );
 
-CREATE INDEX posts_feed_id_idx ON posts(feed_id);
+CREATE INDEX posts_feed_id_idx ON posts (feed_id);
 
-CREATE INDEX posts_crawl_id_idx ON posts(crawl_id);
+CREATE TYPE post_fetch_status AS ENUM ('Success', 'Failure');
+
+CREATE TABLE post_fetches
+(
+    post_fetch_id uuid primary key,
+    post_id       uuid                     not null references posts (post_id),
+    status        post_fetch_status        not null,
+    message       text,
+    fetched_at    timestamp with time zone not null,
+    created_at    timestamp with time zone not null default now()
+);
+
+CREATE TABLE post_summaries
+(
+    post_summary_id  uuid primary key,
+    post_id          uuid                     not null references posts (post_id),
+    summarize_method text                     not null,
+    summary          text                     not null,
+    summarized_at    timestamp with time zone not null,
+    created_at       timestamp with time zone not null default now()
+);
+
+create index post_summaries_post_id_idx ON post_summaries (post_id);
 
 -- +goose Down
 
+DROP TABLE post_summaries;
+DROP TABLE post_fetches;
+DROP TYPE post_fetch_status;
 DROP TABLE posts;
-
-DROP TABLE crawls;
-
-DROP TYPE crawl_status;
-
+DROP TABLE feed_fetches;
+DROP TYPE feed_fetch_status;
 DROP TABLE feeds;
