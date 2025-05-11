8aa73671697732a53dcd3717510d464d2174bc7c
Kotaro Abe <abekoh.bcky@gmail.com>
2025-05-11 15:11:13 +0900

    setup tanstack

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -6,13 +6,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { apolloClient } from "./lib/apollo";
 import { Toaster } from "./components/ui/toaster";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ApolloProvider client={apolloClient}>
       <ChakraProvider value={defaultSystem}>
         <ThemeProvider attribute="class" disableTransitionOnChange>
-          <App />
+          <RouterProvider router={router} />
           <Toaster />
         </ThemeProvider>
       </ChakraProvider>
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,11 @@
-import react from "@vitejs/plugin-react"
-import { resolve } from "path"
-import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+    react(),
+  ],
+});
