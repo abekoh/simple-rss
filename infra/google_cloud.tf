92621c9df98cbcbf63d189ce792a15d20761fb23
Kotaro Abe <abekoh.bcky@gmail.com>
2025-05-12 21:37:39 +0900

    setup backend

--- infra/google_cloud.tf
+++ infra/google_cloud.tf
@@ -1,15 +1,34 @@
 terraform {
   backend "gcs" {
-    bucket  = "abekoh-simple-rss-terraform-backend"
+    bucket = "abekoh-simple-rss-terraform-backend"
   }
 }
 
 provider "google" {
-  project     = "abekoh-simple-rss"
-  region      = "us-west1"
+  project = "abekoh-simple-rss"
+  region  = "us-west1"
 }
 
 resource "google_storage_bucket" "terraform-backend" {
-  name          = "abekoh-simple-rss-terraform-backend"
-  location      = "US-WEST1"
+  name     = "abekoh-simple-rss-terraform-backend"
+  location = "US-WEST1"
+}
+
+resource "google_cloud_run_v2_service" "simple-rss-backend" {
+  name     = "simple-rss-backend"
+  location = "us-west1"
+  ingress  = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+    }
+  }
+}
+
+resource "google_artifact_registry_repository" "simple-rss-backend-repo" {
+  repository_id = "simple-rss-backend"
+  location      = "us-west1"
+  description   = "repository for simple-rss-backend"
+  format        = "DOCKER"
 }
