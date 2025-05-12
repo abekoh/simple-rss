f5558c418d4e5be45ed7892ab1df2e63b25f5ec4
Kotaro Abe <abekoh.bcky@gmail.com>
2025-05-12 22:11:31 +0900

    setup docker

--- infra/google_cloud.tf
+++ infra/google_cloud.tf
@@ -32,3 +32,14 @@ resource "google_artifact_registry_repository" "simple-rss-backend-repo" {
   description   = "repository for simple-rss-backend"
   format        = "DOCKER"
 }
+
+resource "google_service_account" "deploy-account" {
+  account_id   = "simple-rss-deploy"
+  display_name = "Service Account for deploy"
+}
+
+resource "google_project_iam_member" "deploy-member" {
+  project = "abekoh-simple-rss"
+  role    = "roles/editor"
+  member  = "serviceAccount:${google_service_account.deploy-account.email}"
+}
