ca7af804987d849b3291388de5efe7f9fd838b06
Kotaro Abe <abekoh.bcky@gmail.com>
2025-05-17 13:48:44 +0900

    remove unused

--- infra/google_cloud.tf
+++ infra/google_cloud.tf
@@ -14,55 +14,6 @@ resource "google_storage_bucket" "terraform-backend" {
   location = "US-WEST1"
 }
 
-data "google_project" "project" {
-}
-
-variable "DB_URL" {
-  type = string
-}
-
-resource "google_secret_manager_secret" "db-url" {
-  secret_id = "db-url"
-  replication {
-    auto {}
-  }
-}
-
-resource "google_secret_manager_secret_version" "db-url" {
-  secret      = google_secret_manager_secret.db-url.name
-  secret_data = var.DB_URL
-}
-
-resource "google_secret_manager_secret_iam_member" "db-url-access" {
-  secret_id  = google_secret_manager_secret.db-url.id
-  role       = "roles/secretmanager.secretAccessor"
-  member     = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-  depends_on = [google_secret_manager_secret.db-url]
-}
-
-variable "GEMINI_API_KEY" {
-  type = string
-}
-
-resource "google_secret_manager_secret" "gemini-api-key" {
-  secret_id = "gemini-api-key"
-  replication {
-    auto {}
-  }
-}
-
-resource "google_secret_manager_secret_version" "gemini-api-key" {
-  secret      = google_secret_manager_secret.gemini-api-key.name
-  secret_data = var.GEMINI_API_KEY
-}
-
-resource "google_secret_manager_secret_iam_member" "gemini-api-key-access" {
-  secret_id  = google_secret_manager_secret.gemini-api-key.id
-  role       = "roles/secretmanager.secretAccessor"
-  member     = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-  depends_on = [google_secret_manager_secret.gemini-api-key]
-}
-
 resource "google_artifact_registry_repository" "simple-rss-repo" {
   repository_id = "simple-rss"
   location      = "us-west1"
