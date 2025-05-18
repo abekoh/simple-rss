9b3303dcfeb418b0d56cf3dee5087022c33ad782
Kotaro Abe <abekoh.bcky@gmail.com>
2025-05-18 14:32:27 +0900

    fmt

--- infra/neon.tf
+++ infra/neon.tf
@@ -1,16 +1,16 @@
 terraform {
-    required_providers {
-        neon = {
-            source = "kislerdm/neon"
-        }
+  required_providers {
+    neon = {
+      source = "kislerdm/neon"
     }
+  }
 }
 
 provider "neon" {}
 
 resource "neon_project" "simple-rss" {
-  name = "simple-rss"
-  pg_version = 17
-  region_id = "aws-us-west-2"
+  name           = "simple-rss"
+  pg_version     = 17
+  region_id      = "aws-us-west-2"
   store_password = "yes"
 }
