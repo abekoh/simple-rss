2ee0c398c5c8440774912ae82c4360847a53842e
Kotaro Abe <abekoh.bcky@gmail.com>
2026-01-01 11:04:30 +0900

    feat: add singleDB implementation with single connection support
    - Add singleDB type that uses *pgx.Conn instead of connection pool
    - Implement DB interface for singleDB (Queries, Close, Ping)
    - Update Transaction function to support both poolDB and singleDB
    - Add NewSingle constructor for creating single-connection database instances

--- backend/lib/database/database.go
+++ backend/lib/database/database.go
@@ -46,6 +46,34 @@ func (db *poolDB) Ping(ctx context.Context) error {
 	return db.pool.Ping(ctx)
 }
 
+type singleDB struct {
+	conn *pgx.Conn
+}
+
+var _ DB = (*singleDB)(nil)
+
+func NewSingle(ctx context.Context, cnf *config.Config) (*singleDB, error) {
+	conn, err := pgx.Connect(ctx, cnf.DBURL)
+	if err != nil {
+		return nil, err
+	}
+	return &singleDB{
+		conn: conn,
+	}, nil
+}
+
+func (db *singleDB) Close() {
+	db.conn.Close(context.Background())
+}
+
+func (db *singleDB) Queries() *sqlc.Queries {
+	return sqlc.New(db.conn)
+}
+
+func (db *singleDB) Ping(ctx context.Context) error {
+	return db.conn.Ping(ctx)
+}
+
 type txDB struct {
 	tx pgx.Tx
 }
@@ -77,14 +105,23 @@ var ErrTxAlreadyStarted = errors.New("transaction already started")
 
 func Transaction(ctx context.Context, f func(c context.Context) error) error {
 	db := FromContext(ctx)
-	poolD, ok := db.(*poolDB)
-	if !ok {
+
+	var tx pgx.Tx
+	var err error
+
+	switch d := db.(type) {
+	case *poolDB:
+		tx, err = d.pool.Begin(ctx)
+	case *singleDB:
+		tx, err = d.conn.Begin(ctx)
+	default:
 		return ErrTxAlreadyStarted
 	}
-	tx, err := poolD.pool.Begin(ctx)
+
 	if err != nil {
 		return err
 	}
+
 	txD := &txDB{tx: tx}
 	if err := f(WithDB(ctx, txD)); err != nil {
 		rollbackErr := tx.Rollback(ctx)
